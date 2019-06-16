8adfca7352c5c3a24bd417b093c32da13f1ae7b3
Yoyo2324 <49600800+yoyo2324@users.noreply.github.com>
2019-06-16 08:06:45 -0400

    Adding the Unity Project to the repository

---
+++ Assets/Scripts/StarterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarterController : MonoBehaviour
+{
+    public GameObject Gold;
+    public GameObject Iron;
+    public GameObject Aluminum;
+    public GameObject Copper;
+    public GameObject Diamond;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnItems", 1.0f, 1.0f);
+    }
+    
+    void SpawnItems()
+    {
+        
+        Instantiate(Gold, transform.position + new Vector3(0,-0.5f,0), Quaternion.identity);
+    }
+
+    private void OnMouseDown()
+    {
+        print("Clicked on starter");
+    }
+}
